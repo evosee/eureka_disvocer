e963f98a2a2bbe3cd266dd9313c7edce612d1ea1
chensai <chensai123>
2018-10-27 02:15:36 +0800

    my

--- serviceb/src/main/java/com/example/demo/serviceb/ServicebApplication.java
+++ serviceb/src/main/java/com/example/demo/serviceb/ServicebApplication.java
@@ -3,12 +3,35 @@ package com.example.demo.serviceb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+import java.util.Map;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableBinding(Sink.class)
 public class ServicebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServicebApplication.class, args);
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void consume(Message message){
+        System.out.println(message.message+"________________________________________");
+    }
+
+    private static class Message{
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
